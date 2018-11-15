b1c82569f15f903b4985bf86207c2e4d173ee09c
DESKTOP-T644EM4\Kyle <kyle.public10415@gmail.com>
2018-11-15 23:12:43 +0800

    added jquery, modified todo.js, index.html

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,14 @@
 $(document)
     .ready(function () {
 
+        let todoListfilterType = "all"
+        const todoList = [];
+        const buildToDoItem = (element) => `<li id=${element.id} class="${element.complete
+            ? "checked"
+            : ""}"><input name="done-todo" ${element.complete
+                ? 'checked'
+                : ""} type="checkbox" class="done-todo" /><span> ${element.name}</span> </li>`
+
         function generateUUID() {
             /*jshint bitwise:false */
             var i,
@@ -21,5 +29,100 @@ $(document)
             return uuid;
         }
 
-        // code to be implemented
+        function addItem() {
+            var toAdd = $('input[name=MyItem]').val();
+
+            todoList.push({id: generateUUID(), name: toAdd, complete: false});
+
+            GenList();
+
+            $('input[name=MyItem]').val("");
+        }
+
+        function GenList() {
+
+            const filterExecute = (element) => [
+                {
+                    filter: "all",
+                    return: true
+                }, {
+                    filter: "active",
+                    return: !element.complete
+                }, {
+                        filter: "complete",
+                        return: element.complete
+                    }
+                ]
+                .find(element => element.filter === todoListfilterType)
+                .return;;
+            const olHtml = todoList
+                .filter(filterExecute)
+                .map(element => buildToDoItem(element))
+                .reduce((element1, element2) => element1 + element2, "");
+            $('ol').html(olHtml);
+        }
+
+        $('#filters li a')
+            .click(function (e) {
+                e.preventDefault();
+                const filterType = $(this).data('filter');
+                todoListfilterType = filterType;
+                GenList();
+
+                $("#filters li a").removeClass("selected");
+                $(this).addClass("selected");
+
+            });
+
+        $('#button').click(addItem);
+
+        $("input[name=MyItem]").keyup(function (event) {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+                addItem();
+            }
+        });
+
+        $(document).on('click', 'input[name=done-todo]', function (event) {
+            $(this)
+                .parent()
+                .toggleClass('checked');
+
+            todoList
+                .find(element => element.id === $(this).parent()[0].id)
+                .complete = $(this)
+                .parent()
+                .hasClass('checked');
+        });
+
+        $(document).on('dblclick', 'li', function () {
+            $(this)
+                .children('span')
+                .attr('contentEditable', 'true')
+                .focus()
+                .keypress(function (event) {
+                    var keycode = (event.keyCode
+                        ? event.keyCode
+                        : event.which);
+                    if (keycode == '13') {
+                        event
+                            .target
+                            .blur();
+                        $(this)
+                            .children('span')
+                            .attr('contenteditable', 'false');
+
+                        todoList
+                            .find(element => element.id === $(this).parent()[0].id)
+                            .name = $(this).text();
+                        GenList();
+                    }
+                });
+
+        });
+
+        $('input').focus(function () {
+            $(this).val('');
+        });;
+
     });
